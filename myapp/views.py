c98ee1c5c7cc72a32b233240691a7180a5abe3e9
Pablo Céspedes <pablocesar.cespedes98@gmail.com>
2023-10-23 22:29:50 -0600

    Class/URls/Admin
    >Classes creates
    >URLs defined
    >Admin Panel configurated

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,21 @@
-from django.http import HttpResponse
+from django.http import HttpResponse , JsonResponse
+from .models import Project,Task
+from django.shortcuts import get_object_or_404, render
 # Create your views here.
-def hello(request):
-    return HttpResponse("<h2>Hello World</h2>")
+
+def index(request):
+    return render(request,'index.html')
+
+def hello(request,username):
+    print(username)
+    return HttpResponse("<h2>Hello %s</h2>" % username)
 
 def about(request):
     return HttpResponse("About")
+
+def projects(request):
+    projects = list(Project.objects.values())
+    return render(request,'projects.html')
+
+def tasks(request):
+    return render(request,'task.html')
